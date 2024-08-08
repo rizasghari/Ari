eb31060e467946b619370fc959be21c1bff59eb1
rizasghari <developer.rizasghari@gmail.com>
2024-08-08 23:28:45 +0300

    Parser -> return statement implemented withour expression parsing

---
+++ ast/return_statement.go
@@ -0,0 +1,14 @@
+package ast
+
+import "github.com/rizasghari/ari/token"
+
+type ReturnStatement struct {
+	Token       token.Token // the 'return' token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -9,21 +9,15 @@ import (
 
 func TestLetStatements(t *testing.T) {
 	input := `
-	let x  5;
-	let = 10;
-	let 838383;
+	let x = 5;
+	let y = 10;
+	let z = 838383;
 	`
 
 	l := lexer.New(input)
 	p := New(l)
 	program := p.ParseProgram()
 	checkParserErrors(t, p)
-
-	if program != nil {
-		for _, stmt := range program.Statements {
-			t.Logf("TestLetStatements stmt: %v", stmt)
-		}
-	}
 
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
@@ -38,7 +32,7 @@ func TestLetStatements(t *testing.T) {
 	}{
 		{"x"},
 		{"y"},
-		{"foobar"},
+		{"z"},
 	}
 
 	for i, tt := range tests {
@@ -82,4 +76,37 @@ func checkParserErrors(t *testing.T, p *Parser) {
 		t.Errorf("parser error: %q", msg)
 	}
 	t.FailNow()
+}
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+	return 5;
+	return 10;
+	return 993322;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
+				returnStmt.TokenLiteral())
+		}
+	}
 }
